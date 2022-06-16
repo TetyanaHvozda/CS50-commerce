3c47328eaa2b4bb4182b840ea584ee8e4561642d
TetyanaHvozda <62855791+TetyanaHvozda@users.noreply.github.com>
2022-06-16 14:10:56 +0200

    Update admin.py

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Listing, Picture
+from .models import Listing, Picture, Category, Comment, Bid
 
 # Register your models here.
 admin.site.register(Listing)
 admin.site.register(Picture)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Bid)
